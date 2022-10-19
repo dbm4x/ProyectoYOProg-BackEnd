c0dee682ee523d6305b495000c99aa15f67e1361
unknown <roc666@protonmail.com>
2022-10-19 17:59:24 -0300

    first

--- src/main/java/com/monarca/portafolio/controller/Controller.java
+++ src/main/java/com/monarca/portafolio/controller/Controller.java
@@ -30,7 +30,7 @@ import com.monarca.portafolio.service.IPersonaService;
 import com.monarca.portafolio.service.ISkillService;
 import com.monarca.portafolio.service.IProyectoService;
 
-@CrossOrigin(origins = "http://127.0.0.1:4200")
+@CrossOrigin(origins = "https://angular-70dcf.firebaseapp.com", maxAge = 3600)
 @RestController
 @RequestMapping("/api/")
 
